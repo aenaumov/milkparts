6e384e099b943719fd577eea668574885ac4cc8c
Alexander Naumov <a.e.naumov@yandex.ru>
2023-03-01 16:42:22 +0300

    refactor IndexController

--- src/main/java/ru/milkparts/web/controllers/IndexController.java
+++ src/main/java/ru/milkparts/web/controllers/IndexController.java
@@ -2,14 +2,24 @@ package ru.milkparts.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "/index")
+@RequestMapping(path = "/{path}")
 public class IndexController {
 
     @GetMapping
-    public String indexPage(){
-        return "index";
+    public String indexPage(@PathVariable String path) {
+
+        switch (path) {
+            case "index":
+                return "index";
+            case "delivery":
+                return "delivery";
+            default:
+                return "404";
+        }
+
     }
 }
