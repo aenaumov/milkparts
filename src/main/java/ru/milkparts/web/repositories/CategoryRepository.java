9b8b85e42fb49dc71de85ec3296116834a4a9a9c
Alexander Naumov <a.e.naumov@yandex.ru>
2023-03-14 13:13:55 +0300

    create interface CategoryRepository

---
+++ src/main/java/ru/milkparts/web/repositories/CategoryRepository.java
@@ -0,0 +1,14 @@
+package ru.milkparts.web.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.milkparts.web.models.Category;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, String> {
+
+    @Override
+    Optional<Category> findById(String s);
+}
