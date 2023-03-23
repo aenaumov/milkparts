a4a80814b10c1c66bfed3d23397d92fc8ab16d3e
Alexander Naumov <a.e.naumov@yandex.ru>
2023-03-23 17:41:59 +0300

    create ItemPageDTO class

---
+++ src/main/java/ru/milkparts/web/DTOs/ItemPageDTO.java
@@ -0,0 +1,21 @@
+package ru.milkparts.web.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ItemPageDTO {
+
+    private String itemId;
+    private String itemRu;
+    private String itemDescription;
+    private String itemCuttedDescription;
+    private String itemKeywords;
+    private String itemCatCanonicalId;
+    private Boolean itemFirstPage;
+    private String catId;
+    private String catRu;
+    private String catKeywords;
+
+}
