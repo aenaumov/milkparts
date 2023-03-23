a56b622720188ce31af1ad6b3daeda11eb1be825
Alexander Naumov <a.e.naumov@yandex.ru>
2023-03-23 17:44:08 +0300

    create MapperToItemPageDTO class

---
+++ src/main/java/ru/milkparts/web/utils/MapperToItemPageDTO.java
@@ -0,0 +1,26 @@
+package ru.milkparts.web.utils;
+
+import ru.milkparts.web.DTOs.ItemPageDTO;
+import ru.milkparts.web.models.Category;
+import ru.milkparts.web.models.Item;
+
+public final class MapperToItemPageDTO {
+
+    private MapperToItemPageDTO() {
+
+    }
+
+    public static ItemPageDTO toItemPageDTO(Item item,  Category category, String cuttedDescription) {
+        return new ItemPageDTO(
+                item.getItemId(),
+                item.getItemRu(),
+                item.getItemDescription(),
+                cuttedDescription,
+                item.getItemKeywords(),
+                item.getCatCanonicalId(),
+                item.getItemFirstPage(),
+                category.getCatId(),
+                category.getCatRu(),
+                category.getCatKeywords());
+    }
+}
