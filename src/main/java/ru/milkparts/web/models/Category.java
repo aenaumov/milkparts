795dc459f5c80adfd5be1ba82bf7ed203c2ae27b
Alexander Naumov <a.e.naumov@yandex.ru>
2023-03-28 14:20:54 +0300

    refactor

--- src/main/java/ru/milkparts/web/models/Category.java
+++ src/main/java/ru/milkparts/web/models/Category.java
@@ -1,11 +1,29 @@
 package ru.milkparts.web.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.List;
+
+@Entity
+@Table(name = "CATEGORIES")
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PROTECTED, force=true)
 public class Category {
 
+    @Id
+    @Column(name = "CAT_ID")
     private String catId;
+    @Column(name = "CAT_RU")
+    private String catRu;
+    @Column(name = "CAT_DESCRIPTION")
+    private String catDescription;
+    @Column(name = "CAT_KEYWORDS")
+    private String catKeywords;
+
+//    TODO check CascadeType
+    @OneToMany(cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
+    private List<Item> items;
 }
