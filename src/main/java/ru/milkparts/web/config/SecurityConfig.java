59a06ccc63ffa155cf3b146dd81468c2523a90b8
Alexander Naumov <a.e.naumov@yandex.ru>
2023-04-17 16:31:27 +0300

    create method userDetailsService in SecurityConfig

--- src/main/java/ru/milkparts/web/config/SecurityConfig.java
+++ src/main/java/ru/milkparts/web/config/SecurityConfig.java
@@ -2,13 +2,27 @@ package ru.milkparts.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.milkparts.web.models.User;
+import ru.milkparts.web.repositories.UserRepository;
+
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+        return username -> {
+            User user= userRepository.findByUsername(username);
+            if (user != null) return user;
+            throw new UsernameNotFoundException("User ‘" + username + "’ not found");
+        };
     }
 }
