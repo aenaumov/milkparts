88cb87a64551bd01ba3a413b7335dfe2b8a5e23c
Alexander Naumov <a.e.naumov@yandex.ru>
2023-05-17 16:44:45 +0300

    create UserRepository class

--- src/main/java/ru/milkparts/web/repositories/UserRepository.java
+++ src/main/java/ru/milkparts/web/repositories/UserRepository.java
@@ -1,8 +1,12 @@
 package ru.milkparts.web.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.milkparts.web.models.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String  username);
+    User findByUsername(String username);
+
+    User findByEmail(String email);
 }
